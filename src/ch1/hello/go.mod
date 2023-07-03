fc0f77b4088daf13fa9e5e6575f43587dbd547f8
spongeB <guoyubo00737@163.com>
2023-07-03 22:30:25 +0800

    feat: add ch1

---
+++ src/ch1/hello/go.mod
@@ -0,0 +1,3 @@
+module Gone
+
+go 1.20
---
+++ src/ch1/hello/hello_world.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println(os.Args)
+	if len(os.Args) > 1 {
+		fmt.Println("hello world", os.Args[1])
+	}
+	fmt.Println("hello world")
+	// os.Exit(0)
+	os.Exit(-1)
+}
