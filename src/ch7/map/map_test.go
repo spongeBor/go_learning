16c20e1236229a7518b359e7b749d89973885c89
spongeB <guoyubo00737@163.com>
2023-07-05 23:39:00 +0800

    feat: add ch7

---
+++ src/ch7/map/map_test.go
@@ -0,0 +1,34 @@
+package my_map
+
+import "testing"
+
+func TestInitMap(t *testing.T) {
+	m1 := map[int]int{1: 1, 2: 4, 3: 9}
+	t.Log(m1[2])
+	t.Logf("len m1=%d", len(m1))
+	m2 := map[int]int{}
+	m2[4] = 16
+	t.Logf("len m2=%d", len(m2))
+	m3 := make(map[int]int, 10)
+	t.Logf("len m3=%d", len(m3))
+}
+
+func TestAccessMNotExistingKey(t *testing.T) {
+	m1 := map[int]int{}
+	t.Log(m1[1]) //map 不存在也会返回0值
+	m1[2] = 0
+	t.Log(m1[2])
+	// 可以用下面方法区分map值为0还是不存在
+	if v, ok := m1[1]; ok {
+		t.Logf("Key 1's value is %d", v)
+	} else {
+		t.Log("key 1 is not existing.")
+	}
+}
+
+func TestTravelMap(t *testing.T) {
+	m1 := map[int]int{1: 1, 2: 4, 3: 9}
+	for k, v := range m1 {
+		t.Log(k, v)
+	}
+}
