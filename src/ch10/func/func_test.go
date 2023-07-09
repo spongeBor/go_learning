b0d0bbbcd40c4491888f0b2cfc05bc2c8ccf9d50
spongeB <guoyubo00737@163.com>
2023-07-09 15:11:33 +0800

    feat: add defer

--- src/ch10/func/func_test.go
+++ src/ch10/func/func_test.go
@@ -33,3 +33,27 @@ func TestFn(t *testing.T) {
 	tsSF := timeSpent(slowFun)
 	t.Log(tsSF(10))
 }
+
+func Sum(ops ...int) int {
+	ret := 0
+	for _, op := range ops {
+		ret += op
+	}
+	return ret
+}
+
+func TestValParam(t *testing.T) {
+	t.Log(Sum(1, 2, 3, 4))
+	t.Log(Sum(1, 2, 3, 4, 5))
+}
+
+func Clear() {
+	fmt.Println("Clear resources.")
+}
+
+func TestDefer(t *testing.T) {
+	defer Clear() // 相当于setTimeout 的行为 ；panic时候也会执行
+	fmt.Println("Start")
+	// panic("err")
+	fmt.Println("End")
+}
